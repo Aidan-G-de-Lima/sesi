7cbfbbf1b325eb716706ddbce6a899faac82d033
Aidan-G-de-Lima <aidan.lima@aluno.senai.br>
2025-05-28 20:16:23 -0300

    Add files via upload

---
+++ cadastrar_palestra.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h2>Bem-vindo a nossa instituição com sua apresentação</h2>";
+echo "<p>Palestrante: ". $_POST['palestrante'] ."</p>";
+echo "<p>Nome: ". $_POST['nome'] ."</p>";
+echo "<p>Descrição: ". $_POST['descricao'] ."</p>";
+echo "Volte a outra pagina e reinicie ela para aparecer sua palestra";
+?>
---
+++ enviar_contato.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2>Mensagem Enviada com Sucesso!</h2>";
+echo "<p>Nome: ". $_POST['nome'] ."</p>";
+echo "<p>Email: ". $_POST['email'] ."</p>";
+echo "<p>Mensagem: ". $_POST['mensagem'] ."</p>";
+?>
