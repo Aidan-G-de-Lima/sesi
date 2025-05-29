78e45dea113405fd4c4b64233e9ea3acdbe6c87a
Aidan-G-de-Lima <aidan.lima@aluno.senai.br>
2025-05-29 08:54:53 -0300

    termino

--- cadastrar_participante.php
+++ cadastrar_participante.php
@@ -3,7 +3,6 @@ echo "<h2>Cadastro de Participante Realizado com Sucesso!</h2>";
 echo "<p>Nome: ". $_POST['nome'] ."</p>";
 echo "<p>Idade: ". $_POST['idade'] ."</p>";
 echo "<p>Telefone: ". $_POST['telefone'] ."</p>";
-echo "<p>Palestra: ". $_POST['palestra'] ."</p>";
 echo "<p>Tipo: ". $_POST['tipo'] ."</p>";
 if ($_POST['tipo'] == 'Aluno') {
   echo "<p>RM: ". $_POST['rm'] ."</p>";
